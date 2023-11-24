20dd49df0f71d7a0c0ceef4dcc7ff4ef6e49ab1f
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-24 21:13:08 +0530

    Add project files.

---
+++ SF.Mod37.XamarinHomeApp/SF.Mod37.XamarinHomeApp/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SF.Mod37.XamarinHomeApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+		}
+		private void Login_Click(object sender, EventArgs e)
+		{
+
+           string xaml = "<Button Text=\"⌛ Attempting login...\"  />";
+           loginButton.LoadFromXaml(xaml);
+
+		}
+	}
+}
